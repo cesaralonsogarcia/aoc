bf22442d7058105149e7b6c57fb85f8de97e18e4
Cesar A Garcia <cesar.garcia@centeredsolutions.com>
2025-01-23 11:51:53 -0600

    finished solution to problem7-1

--- 2024/problems/problem7-1.py
+++ 2024/problems/problem7-1.py
@@ -12,59 +12,29 @@ def main():
         line = []
         operation = []
         possibilities = []
+        total = []
         for e in l:
             number = e.strip(':')
             line.append(number)
         result = int(line.pop(0))
-        print(f'result: {result}')
         possibilities = list(product(operators, repeat=len(line)-1))
-        # print(line)
-        # print(possibilities, len(possibilities))
         if len(possibilities) == 2:
             possibilities = operators
-        # print(possibilities)
         for equation in possibilities:
-            calculation = ''
+            calculation = int(line[0])
             for i in range(len(equation)):
-                # print(line[i], equation[i])
-                calculation += line[i] + equation[i]
-            calculation += line[i + 1]
-            total = eval(calculation)
-            if total == result:
-                print(f'total: {total} result: {result}')
-                calibration += total
+                if equation[i] == '+':
+                    calculation += int(line[i + 1])
+                else:
+                    calculation = calculation * int(line[i + 1])
+            if calculation == result:
+                if calculation in total:
+                    break
+                else:
+                    total.append(calculation)
+                    print(f'calculation: {calculation} result: {result}')
+                    calibration += calculation
     print(calibration)
-    print(len(input))
-        # for i in range(1):
-        #     for p in possibilities:
-        #         for sign in p:
-        #             print(sign)
-        #             total = line[0]
-        #             for n in range(1, len(line)):
-        #                 print(line[n])
-        #                 print(f'p: {p}')
-        #                 if '+' in sign:
-        #                     total += line[n]
-        #                 if '*' in sign:
-        #                     total = total * line[n]
-        #             print(f'total: {total}')       
-
-                    
-        # print(possibilities)
-        # for i in range(possibilities):
-        #     for element in range(1, len(line)):
-        #         equation =
-    
-    
-    
-    # total = number[0]
-    # for number in numbers: maybe use index
-    # for sign in signs:
-    # if sign == '+'
-    # total += next(number)
-    # if sign == '*'
-    # total = total * next(number)
-    # check if total against result
 
 main()
 
